4e25cbaa140947647139ac01bf458c472cf4dd05
Sophie Mann <sklm@live.co.uk>
2017-01-25 11:50:21 +0000

    add to repo

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title=(new)
+		words = new.split.map do |word|
+			unless %w(and in the of a an).include?(word)
+				word.capitalize
+			else
+				word
+			end
+		end
+		words[0].capitalize!
+		@title = words.join(" ")
+	end
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize 
+        @seconds = 0
+  end
+  
+  #def time_string
+  #     Time.at(@seconds).utc.strftime("%H:%M:%S")
+  #end
+  
+  def time_string
+        seconds = @seconds % 60
+        minutes = (@seconds / 60) % 60
+        hours = @seconds / (60 * 60)
+        format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
+  
 end
+  
